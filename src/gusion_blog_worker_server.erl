aaa2784fdac851a6314fc7d304e15be14be09cb4
Solomon Wu <solomon.wzs@gmail.com>
2013-08-01 18:33:59 +0800

    modified:   src/gusion_blog_worker_server.erl

--- src/gusion_blog_worker_server.erl
+++ src/gusion_blog_worker_server.erl
@@ -12,11 +12,41 @@
 
 init([Dir, Name])->
     process_flag(trap_exit, true),
-
     StateFileName=filename:absname_join(Dir, Name++'.bsta'),
     {ok, Bin}=file:read_file(StateFileName),
-    BLogState=binary_to_term(Bin),
+    {WFileLog, NewBLogState}=swap_wfile(Dir, binary_to_term(Bin)),
+    {ok, #state{dir=Dir, blog_state=NewBLogState, wfile_log=WFileLog}}.
+
+handle_call({write_blog, Term}, _From, State)->
+    Reply=case disk_log:blog(State#state.wfile_log, term_to_binary(Term)) of
+        ok->ok;
+        {error, Reason}->{aborted, Reason}
+    end,
+    {ok, Reply, State};
+handle_call(_Msg, _From, State)->
+    {reply, reply, State}.
+
+handle_cast(_Msg, State)->
+    {noreply, State}.
+
+handle_info(swap_wfile, State)->
+    #state{dir=Dir, blog_state=BLogState, wfile_log=WFileLog}=State,
+    disk_log:close(WFileLog),
+    {NewWFileLog, NewBLogState}=swap_wfile(Dir, BLogState),
+    {noreply, State#state{wfile_log=NewWFileLog, blog_state=NewBLogState}};
+handle_info(_Msg, State)->
+    {noreply, State}.
+
+code_change(_Vsn, State, _Extra)->
+    {ok, State}.
+
+terminate(_Reason, State)->
+    disk_log:close(State#state.wfile_log),
+    ok.
+
+swap_wfile(Dir, BLogState)->
     #gusion_blog_state{name=Name, wfile=WFile, pfiles=PFiles}=BLogState,
+    StateFileName=filename:absname_join(Dir, Name++'.bsta'),
 
     NewWFile=Name++"_"++integer_to_list(?timestamp),
     {ok, WFileLog}=disk_log:open([{name, NewWFile++".bdat"},
@@ -31,20 +61,4 @@ init([Dir, Name])->
     NewBLogState=BLogState#gusion_blog_state{wfile=NewWFile, pfiles=NewPFiles},
     file:write_file(StateFileName, term_to_binary(NewBLogState)),
 
-    {ok, #state{dir=Dir, blog_state=NewBLogState, wfile_log=WFileLog}}.
-
-handle_call(_Msg, _From, State)->
-    {reply, reply, State}.
-
-handle_cast(_Msg, State)->
-    {noreply, State}.
-
-handle_info(_Msg, State)->
-    {noreply, State}.
-
-code_change(_Vsn, State, _Extra)->
-    {ok, State}.
-
-terminate(_Reason, State)->
-    disk_log:close(State#state.wfile_log),
-    ok.
+    {WFileLog, NewBLogState}.
