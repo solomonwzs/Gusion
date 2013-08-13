8dbb5d4df34e7d5152fbce148c9fa07b4db89148
Solomon Wu <solomon.wzs@gmail.com>
2013-08-13 17:38:56 +0800

    modified:   src/gusion_blog_worker_server.erl

--- src/gusion_blog_worker_server.erl
+++ src/gusion_blog_worker_server.erl
@@ -7,12 +7,16 @@
 -record(state, {
         dir::string(),
         blog_state::record(gusion_blog_state),
-        wfile_log::log()
+        wfile_log::log(),
+        finding_idle_iter::pid()|nil
     }).
+-define(GET_ITER_PROCESS_TIMEOUT, 500).
 
 -define(write_state_file(Dir, Name, BLogState),
     file:write_file(filename:absname_join(Dir, ?state_file_name(Name)),
         BLogState)).
+-define(sup_children(Name), supervisor:which_children(list_to_atom(
+            ?iter_sup_name(Name)))).
 
 init([Dir, Name])->
     process_flag(trap_exit, true),
@@ -20,6 +24,7 @@ init([Dir, Name])->
     {ok, Bin}=file:read_file(StateFileName),
     {WFileLog, NewBLogState}=swap_wfile(Dir, binary_to_term(Bin)),
     {ok, #state{
+            finding_idle_iter=nil,
             dir=Dir,
             blog_state=NewBLogState,
             wfile_log=WFileLog}}.
@@ -45,10 +50,68 @@ handle_call({remove_process_blog, PFile}, _From, State)->
         pfiles=?set_del_element(PFile, PFiles)},
     ok=?write_state_file(Dir, Name, BLogState),
     {reply, ok, State#state{blog_state=NewBLogState}};
+handle_call({new_process_task, nil}, {Finding, _}, State=#state{
+        blog_state=BLogState,
+        finding_idle_iter=Finding
+    })->
+    if
+        BLogState#gusion_blog_state.ifiles=/=[]->
+            gen_server:cast(self(), get_idle_iterator);
+        true->ok
+    end,
+    {reply, ok, State#state{finding_idle_iter=nil}};
+handle_call({new_process_task, Iterator}, {Finding, _}, State=#state{
+        dir=Dir,
+        blog_state=BLogState,
+        finding_idle_iter=Finding
+    })->
+    #gusion_blog_state{
+        name=Name,
+        ifiles=IFiles,
+        pfiles=PFiles,
+        pro_func=ProFunc,
+        chunk_size=ChunkSize
+    }=BLogState,
+    {NewPFile, NewIFiles}=?set_pull_element(IFiles),
+    if
+        NewPFile=:=nil->
+            {reply, ok, State#state{finding_idle_iter=nil}};
+        true->
+            ok=gen_server:call(Iterator,
+                {new_task, Dir, NewPFile, ProFunc, ChunkSize}),
+            NewBLogState=BLogState#gusion_blog_state{
+                ifiles=NewIFiles,
+                pfiles=?set_add_element(NewPFile, PFiles)
+            },
+            ok=?write_state_file(Dir, Name, NewBLogState),
+            {reply, ok, State#state{
+                    blog_state=BLogState,
+                    finding_idle_iter=nil}}
+    end;
+handle_call({new_process_task, _}, _From, State)->
+    Finding=State#state.finding_idle_iter,
+    if
+        is_pid(Finding)->
+            case process_info(Finding) of
+                undefined->{reply, ok, State#state{finding_idle_iter=nil}};
+                _->{reply, ok, State}
+            end;
+        true->{reply, ok, State#state{finding_idle_iter=nil}}
+    end;
 handle_call(_Msg, _From, State)->
     {reply, reply, State}.
 
-%handle_cast({compiled, PFile}, State)->
+handle_cast(get_idle_iterator, State=#state{
+        finding_idle_iter=nil,
+        blog_state=BLogState
+    })->
+    Self=self(),
+    Pid=spawn(fun()->
+                Iterator=get_idle_iterator(
+                    ?sup_children(BLogState#gusion_blog_state.name)),
+                gen_server:call(Self, {new_process_task, Iterator})
+        end),
+    {noreply, State#state{finding_idle_iter=Pid}};
 handle_cast(_Msg, State)->
     {noreply, State}.
 
@@ -73,17 +136,6 @@ terminate(_Reason, State)->
     disk_log:close(State#state.wfile_log),
     ok.
 
-%new_process_task(State=#state{
-%        dir=Dir,
-%        blog_state=BLogState
-%    })->
-%    #gusion_blog_state{
-%        name=Name,
-%        pfiles=PFiles,
-%        func=Func
-%    }=BLogState,
-%    PFiles=
-
 swap_wfile(Dir, BLogState)->
     #gusion_blog_state{
         name=Name,
@@ -102,6 +154,17 @@ swap_wfile(Dir, BLogState)->
     ok=disk_log:close(Ret),
 
     NewBLogState=BLogState#gusion_blog_state{wfile=NewWFile, ifiles=NewIFiles},
-    ok=?write_state_file(Dir, Name, BLogState),
+    ok=?write_state_file(Dir, Name, NewBLogState),
 
     {WFileLog, NewBLogState}.
+
+get_idle_iterator([])->nil;
+get_idle_iterator([{_Id, Child, _Type, _Module}|Tail])->
+    try
+        case gen_server:call(Child, get_progress, ?GET_ITER_PROCESS_TIMEOUT) of
+            idle->{ok, Child};
+            _->get_idle_iterator(Tail)
+        end
+    catch
+        _:_->get_idle_iterator(Tail)
+    end.
